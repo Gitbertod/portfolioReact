de082c56208b94c60f53adfabdb9611a879cc043
Gilberto Diaz <gilbertodiaz@MacBook-Air-de-Lorena.local>
2024-06-20 14:38:48 +0200

    making some styles

--- my-project/src/components/ContainerComponent.jsx
+++ my-project/src/components/ContainerComponent.jsx
@@ -2,8 +2,18 @@
 
 const ContainerComponent = () => {
   return (
-    <section className="h-96 bg-[url('src/assets/common-bg.svg')] my-26  opacity-25">
+    <section className="flex items-center justify-center h-dvh bg-[url('src/assets/common-bg.svg')] my-26  opacity-25">
+      <div className="w-1/2 flex items-center justify-center content-center space-x-4">
+        <div className="">
+          <h1 className="w-64 text-[52px]">Hola, soy <br></br>Gilberto Diaz</h1>
+        </div>
+        <div>
+          <p className="text-[20px]">Desarrollador front-end y de videojuegos, creo aplicaciones web intuitivas
+            funcionales y atractivas
+          </p>
+        </div>
 
+      </div>
     </section>
   )
 }
